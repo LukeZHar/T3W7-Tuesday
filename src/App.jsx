5fe071bdeb89ef28d6d67f7c44aafb73abca5cb6
LukeZHar <15649@coderacademy.edu.au>
2024-11-27 23:22:36 +1000

    Feat: lifecycle methos implemented, API request made

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,50 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// Lifestyle components
+// componentDidMount() {} - Loads when ther website initially loads
+// componentDidUpdate() {} - Loads when there is a change
+// componentWillUnmount() {} - Loads when the component is collapsed
+
 function App() {
   const [count, setCount] = useState(0)
+  const [randomWord, setWord] = useState('abc')
+  const [pokemonName, setPokemonName] = useState("")
+  const [pokemonImage, setPokemonImage] = useState("")
+
+  const getPokemonData = async () => {
+    setPokemonName(response.name);
+    setPokemonImage(response.sprites.front_default);
+
+    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${Math.ceil(Math.random() * 1025)}`).then(response => response.json())
+  }
+
+
+
+  //  Works like componentDidMount()
+  //  useEffect(callback, dependencyArray)
+  // dependencyArray - whenever something changes here the useEffect hook runs
+  useEffect(() => {
+    console.log("Component Mounted")
+
+    getPokemonData();
+
+    // Works like componentWillUnmount()
+    return (() => {
+      console.log("Component will unmount now.")
+    })
+  }, [])
+
+  useEffect(() => {
+    console.log(`count: ${count}`)
+  }, [count, randomWord])
+
+  useEffect(() => {
+    console.log(`Pokemon Data retrieved, Name: ${pokemonName}, Image: ${pokemonImage}`)
+  }, [pokemonName, pokemonImage])
+
 
   return (
     <>
